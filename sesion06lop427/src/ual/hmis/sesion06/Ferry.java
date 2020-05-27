b24e77f7be9f31e0d7be8dbbd171e6d964fb52fb
NICOLAS ORTEGA <lop427@inlumine.ual.es>
2020-05-27 03:40:55 +0200

    cambios sesion06

--- sesion06lop427/src/ual/hmis/sesion06/Ferry.java
+++ sesion06lop427/src/ual/hmis/sesion06/Ferry.java
@@ -5,111 +5,107 @@ import java.util.ArrayList;
 public class Ferry {
 
 	private int numMaxPasajeros;
-	private int numTotalVehiculos;
 	private int pesoMaxVehiculos;
-	private int numTotalPasajeros;
-	private int pesoTotalVehiculos;
 	private int numMaxVehiculos;
-	private ArrayList<Vehiculo> vehiculos;
+	private int numVehiculos;
+	private int numPersonas;
+	private int pesoVehiculos;
+
+	ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+
 	
-	public Ferry() {
-		this.numTotalPasajeros = 0;
-		this.numTotalVehiculos = 0;
-		this.pesoTotalVehiculos = 0;
-		this.vehiculos = new ArrayList<Vehiculo>();
+	public Ferry(final int numMaxVehiculos, final int numMaxPasajeros,
+		final int pesoMaxVehiculos, final int personas,
+		final int vehiculos, final int pesoVehiculos) {
+		this.numPersonas = personas;
+		this.numVehiculos = vehiculos;
+		this.pesoVehiculos = pesoVehiculos;
+		this.numMaxVehiculos = numMaxVehiculos;
+		this.numMaxPasajeros = numMaxPasajeros;
+		this.pesoMaxVehiculos = pesoMaxVehiculos;
 	}
 	
-		// Embarca un vehículo añadiéndolo a la lista
-		public boolean embarcarVehiculo (Vehiculo v) {
-			this.pesoTotalVehiculos = this.pesoTotalVehiculos + v.getPeso();
-			this.numTotalPasajeros = this.numTotalPasajeros + v.getNumPasajeros();
+	public int getNumMaxPasajeros() {
+		return numMaxPasajeros;
+	}
+	
+	public void setNumMaxPasajeros(final int numMaxPasajeros) {
+		this.numMaxPasajeros = numMaxPasajeros;
+	}
 
-			return vehiculos.add(v);
-		}
+	
+	public int getPesoMaxVehiculos() {
+		return pesoMaxVehiculos;
+	}
+
+	public void setPesoMaxVehiculos(final int pesoMaxVehiculos) {
+		this.pesoMaxVehiculos = pesoMaxVehiculos;
+	}
+	
+	public int getNumMaxVehiculos() {
+		return numMaxVehiculos;
+	}
+	
+	public void setNumMaxVehiculos(final int numMaxVehiculos) {
+		this.numMaxVehiculos = numMaxVehiculos;
+	}
+	
+	public int getNumVehiculos() {
+		return numVehiculos;
+	}
+
+	public void setNumVehiculos(final int numVehiculos) {
+		this.numVehiculos = numVehiculos;
+	}
+	
+	public int getNumPersonas() {
+		return numPersonas;
+	}
+	
+	public void setNumPersonas(final int numPersonas) {
+		this.numPersonas = numPersonas;
+	}
+	
+	public int getPesoVehiculos() {
+		return pesoVehiculos;
+	}
+	
+	public void setPesoVehiculos(final int pesoVehiculos) {
+		this.pesoVehiculos = pesoVehiculos;
+	}
+
+	public ArrayList<Vehiculo> getVehiculos() {
+		return vehiculos;
+	}
+
+	public void setVehiculos(final ArrayList<Vehiculo> vehiculos) {
+		this.vehiculos = vehiculos;
+	}
 		
-		// devuelve el número total de vehículos embarcados
-		public int totalVehiculos() {
-			return getNumTotalVehiculos();
+	public boolean embarcarVehiculo(Vehiculo v) {
+		this.pesoVehiculos = this.pesoVehiculos + v.peso;
+		this.numPersonas = this.numPersonas + v.numPasajerosVehi;
+		return vehiculos.add(v);
+	}
+	
+	public int totalVehiculos() {
+		return vehiculos.size();
+	}
+	
+	public boolean vacio() {
+		return vehiculos.size() == 0;
+	}
+	
+	public boolean superadoMaximoVehiculos() {
+		return vehiculos.size() > this.numMaxVehiculos;
+	}
+	
+	public boolean superadoMaximoPeso() {
+		int peso = 0;
+		for (Vehiculo v: vehiculos) {
+			peso += v.peso;
 		}
-		
-		// verdadero si no hay ningún vehículo
-		public boolean vacio() {
-			if (vehiculos.isEmpty()) return true;
-			return false;
-		}
-		
-		// verdadero si el número total de los vehículos supera el máximo
-		public boolean superadoMaximoVehiculos() {
-			if (this.vehiculos.size() > numMaxVehiculos) return true;
-			return false;
-		}
-		
-		// verdadero si el peso total de los vehículos supera el máximo
-		public boolean superadoMaximoPeso() {
-			if (pesoTotalVehiculos > pesoMaxVehiculos) return true;
-			return false;
-		}
-		
-		// -----------------GETTERS Y SETTERS --------------------------
-		
-		
-		public int getNumMaxPasajeros() {
-			return numMaxPasajeros;
-		}
+		return peso > this.pesoMaxVehiculos;
+	}
 
-		public void setNumMaxPasajeros(int numMaxPasajeros) {
-			this.numMaxPasajeros = numMaxPasajeros;
-		}
-
-		public int getNumTotalVehiculos() {
-			return numTotalVehiculos;
-		}
-
-		public void setNumTotalVehiculos(int numTotalVehiculos) {
-			this.numTotalVehiculos = numTotalVehiculos;
-		}
-
-		public int getPesoMaxVehiculos() {
-			return pesoMaxVehiculos;
-		}
-
-		public void setPesoMaxVehiculos(int pesoMaxVehiculos) {
-			this.pesoMaxVehiculos = pesoMaxVehiculos;
-		}
-
-		public int getNumTotalPasajeros() {
-			return numTotalPasajeros;
-		}
-
-		public void setNumTotalPasajeros(int numTotalPasajeros) {
-			this.numTotalPasajeros = numTotalPasajeros;
-		}
-
-		public int getPesoTotalVehiculos() {
-			return pesoTotalVehiculos;
-		}
-
-		public void setPesoTotalVehiculos(int pesoTotalVehiculos) {
-			this.pesoTotalVehiculos = pesoTotalVehiculos;
-		}
-
-		public ArrayList<Vehiculo> getVehiculos() {
-			return vehiculos;
-		}
-
-		public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
-			this.vehiculos = vehiculos;
-		}
-
-		public int getNumMaxVehiculos() {
-			return numMaxVehiculos;
-		}
-
-		public void setNumMaxVehiculos(int numMaxVehiculos) {
-			this.numMaxVehiculos = numMaxVehiculos;
-		}
-		
-		
-		
-		
 }
--- sesion06lop427/src/ual/hmis/sesion06/Vehiculo.java
+++ sesion06lop427/src/ual/hmis/sesion06/Vehiculo.java
@@ -2,39 +2,14 @@ package ual.hmis.sesion06;
 
 public class Vehiculo {
 
-	private int numPasajeros;
-	private int numRuedas;
-	private int peso;
-	
-	public Vehiculo(int peso, int numPasajeros, int numRuedas) {
-		this.peso = peso;
-		this.numPasajeros = numPasajeros;
+	int numPasajerosVehi;	
+	int numRuedas;
+	int peso;
+
+	public Vehiculo(int numPasajeros, int numRuedas, int peso) {
+		super();
+		this.numPasajerosVehi = numPasajeros;
 		this.numRuedas = numRuedas;
-	}
-	
-	public int getNumPasajeros() {
-		return numPasajeros;
-	}
-
-	public void setNumPasajeros(int numPasajeros) {
-		this.numPasajeros = numPasajeros;
-	}
-
-	public int getNumRuedas() {
-		return numRuedas;
-	}
-
-	public void setNumRuedas(int numRuedas) {
-		this.numRuedas = numRuedas;
-	}
-
-	public int getPeso() {
-		return peso;
-	}
-
-	public void setPeso(int peso) {
 		this.peso = peso;
 	}
-	
-	
 }
